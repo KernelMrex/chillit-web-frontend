ff6ba8579e5d7a2a9af2cd019840831cd21b0966
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2020-04-18 15:18:52 +0300

    Setted up workspace. Configured SASS. Started main screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
 
 function App() {
-  return (
-      <div>
-        <h1>Chill-it React frontend</h1>
-      </div>
-  );
+    return (
+        <div>
+            <h1>Chillit React frontend</h1>
+        </div>
+    );
 }
 
 export default App;
