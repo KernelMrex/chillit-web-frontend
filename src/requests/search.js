aaa3cbd2d87e69a5d40703c498f5402726669d02
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2020-05-22 00:09:03 +0300

    Implemented choose city button

--- src/requests/search.js
+++ src/requests/search.js
@@ -30,7 +30,34 @@ export function searchPlaces(query, cityID, offset = 0, amount = 2) {
     });
 }
 
-function buildURLQuery(urlString, params) {
+export function getCities(offset = 0, amount = 20) {
+    return new Promise((resolve, reject) => {
+        let url = buildURLQuery("http://" + PLACES_STORE_URL + "/cities", {
+            "amount": amount,
+            "offset": offset,
+        });
+        fetch(url, {
+            method: "GET",  
+        }).then((result) => {
+            if (result.status !== 200) {
+                reject("bad returned status code " + result.status);
+                return;
+            }
+            result.json().then((result) => {
+                resolve(result.cities);
+                return;
+            }, (error) => {
+                reject(error);
+                return;
+            });
+        }, (error) => {
+            reject(error);
+            return;
+        });
+    });
+}
+
+function buildURLQuery(urlString, params = []) {
     let url = new URL(urlString);
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
     return url;
